d0599f31844d309613045a1e0bb08ac7316e5a9e
Beatriz Santos <beatriz.vbps@gmail.com>
2019-07-10 16:18:48 +0100

    Oversights (overseer and oversee) and EmailAddresses (participantType) mappings changed

--- app/repositories/slick/mappings/EmailAddressesTable.scala
+++ app/repositories/slick/mappings/EmailAddressesTable.scala
@@ -3,14 +3,14 @@ package repositories.slick.mappings
 
 import slick.jdbc.MySQLProfile.api._
 
-case class EmailAddressRow (emailAddressId: Int, emailId: Int, addressId: Int, receiverType: String)
+case class EmailAddressRow (emailAddressId: Int, emailId: Int, addressId: Int, participantType: String)
 
 class EmailAddressesTable (tag: Tag) extends Table[EmailAddressRow](tag, "email_addresses") {
 	// Columns
 	def emailAddressId = column[Int]("email_address_id", O.PrimaryKey, O.AutoInc)
 	def emailId = column[Int]("email_id")
 	def addressId = column[Int]("address_id")
-	def receiverType = column[String]("receiver_type")
+	def participantType = column[String]("participant_type")
 	
 	
 	// Indexes
@@ -18,7 +18,7 @@ class EmailAddressesTable (tag: Tag) extends Table[EmailAddressRow](tag, "email_
 	
 	// Table mapping
 	override def * =
-		(emailAddressId, emailId, addressId, receiverType) <> (EmailAddressRow.tupled, EmailAddressRow.unapply)
+		(emailAddressId, emailId, addressId, participantType) <> (EmailAddressRow.tupled, EmailAddressRow.unapply)
 	
 }
 
